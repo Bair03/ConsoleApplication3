06e3d7d6d3cb114129f4ff896c21e87923dd8b08
Bair <bair030805@gmail.com>
2021-09-29 12:47:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, x, y, z;
+    if (a >= x) {
+        if (b >= y) {
+            cout << "YES" << endl;
+        }
+        else if (b>=z) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    else if (b >= x) {
+        if (a >= y) {
+            cout << "YES" << endl;
+        }
+        else if (a >= z) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    else if (a >= y) {
+        if (b >= z) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    else if (b >= y) {
+        if (a >= z) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }else{
+        cout << "NO" << endl;
+    }
+}
